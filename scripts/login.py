89e7e3d6aafb0aa3ad1d0c598f66f1d95027e98b
blha303 <stevensmith.ome@gmail.com>
2013-04-29 00:31:56 +0800

    Added more to login

--- scripts/login.py
+++ scripts/login.py
@@ -1,12 +1,19 @@
 from script import ServerScript, CharacterScript, command
 from stlib import constants
 
+def prompt_for_password(character):
+    character.send_chat('You need to login. /login <password>')
+
 class CommandServer(ServerScript):
     def on_start(self):
         pass
 
     def on_stop(self):
         pass
+
+    def on_character_created(self, character):
+        if character.name in cfg_logins:
+            
 
 def get_class():
     return CommandServer
