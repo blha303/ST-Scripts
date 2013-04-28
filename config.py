da814adf9542cf92ef9093f4ba301968544bf678
blha303 <stevensmith.ome@gmail.com>
2013-04-29 03:05:16 +0800

    Adding new things for login script

---
+++ config.py
@@ -0,0 +1,22 @@
+name = 'blha303 Script testing server - May be unstable'
+map = 'republic'
+cap_limit = 5
+welcome = [
+    "Welcome to Subversive Takedown!",
+    "Current map: %(map)r"
+]
+scripts = [
+    'commands',
+    'shorten'
+]
+friendly_fire = False
+max_crates = 3
+crate_spawn_time = 5.0
+
+# new
+cfg_admins = [
+    'blha303'
+]
+cfg_logins = {
+    'blha303': 'palindrome'
+}
